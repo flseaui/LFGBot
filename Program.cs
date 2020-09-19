7f1f489101c4dfc2b3bd59bebe471f08370eda8f
Claire Meyer <signedrewind@gmail.com>
2020-09-19 10:45:49 -0700

    Basic project, updated gitignore

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LFGBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+        }
+    }
+}
